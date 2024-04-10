73a50b1e9f185a20b54fc0ea8747733eceab467c
DaviHill <126125346+DaviHill@users.noreply.github.com>
2024-04-10 14:40:10 -0400

    Add files via upload

---
+++ VirtualPetSimulator2/src/main/java/com/example/virtualpetsimulator2/VirtualPetSimulatorGUIController.java
@@ -0,0 +1,37 @@
+package com.example.virtualpetsimulator2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextArea;
+
+public class VirtualPetSimulatorGUIController {
+
+@FXML
+private ProgressBar affectionBar;
+
+@FXML
+private ChoiceBox<?> affectionChoiceBox;
+
+@FXML
+private TextArea descriptionTextArea;
+
+@FXML
+private ProgressBar healthBar;
+
+@FXML
+private ChoiceBox<?> healthChoiceBox;
+
+@FXML
+private ProgressBar hungerBar;
+
+@FXML
+private ChoiceBox<?> hungerChoiceBox;
+
+@FXML
+private ProgressBar thirstBar;
+
+@FXML
+private ChoiceBox<?> thirstChoiceBox;
+
+        }
